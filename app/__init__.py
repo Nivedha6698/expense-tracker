2d2421a1feb00e8e0cf37d5db89100bf621fdb11
nivedha6698@gmail.com <nivedha6698@gmail.com>
2026-04-03 14:34:33 +0530

    changes performed for RDS AWS connection on _init__config

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+import os
+
 
 db = SQLAlchemy()
 jwt = JWTManager()
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
 
 def create_app():
     app = Flask(__name__)
+    
+
+    #app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+
 
     # Config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'super-secret-key'
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db
 from datetime import datetime
 
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
