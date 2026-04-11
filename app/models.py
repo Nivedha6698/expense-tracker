ffcd66042d25db26f48068cea779ea6cb4ea6c0a
nivedha6698@gmail.com <nivedha6698@gmail.com>
2026-04-11 18:03:10 +0530

    changed user table name in models.py, build run failed'

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
